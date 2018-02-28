55e5aa4c5eb94c6f99f7c520b3264d02de7fbc91
Enimiste <enimiste@users.noreply.github.com>
2018-02-28 03:50:34 +0000

    Test db access fron index action

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,16 +3,25 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import play.api.db.Database
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(db: Database, cc: ControllerComponents) extends AbstractController(cc) {
 
  
   def index() = Action { implicit request: Request[AnyContent] =>
+     db withConnection { conn =>
+        val stmt = conn.createStatement
+        val rs = stmt.executeQuery("SELECT name FROM tests")
+
+        while (rs.next()) {
+          println(rs.getString("name"))
+        }
+      }
       Ok(views.html.index())
   }
   
